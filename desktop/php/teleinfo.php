969826e86936da6915ce268eedb2541122f7cfa4
ticed35 <ticed35@hotmail.com>
2018-02-19 08:54:39 -0500

    Problème affichage objet

--- desktop/php/teleinfo.php
+++ desktop/php/teleinfo.php
@@ -100,139 +100,143 @@ if($controlerState === ''){
 		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
 			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
       </br>
-				<div class="row">
-					<div class="col-lg-6">
-						<form class="form-horizontal">
-							<fieldset>
-								<div class="form-group">
-									<label class="col-lg-4 control-label">{{Nom de l'équipement}} :</label>
-									<div class="col-lg-4">
-										<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-										<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}"/>
-									</div>
-                  <div class="col-lg-4"/>
-								</div>
-								<div class="form-group">
-									<label class="col-lg-4 control-label" >{{Objet parent :}}</label>
-									<div class="col-lg-4">
-										<select class="eqLogicAttr form-control" data-l1key="object_id">
-											<option value="">{{Aucun}}</option>
-											<?php
-											foreach (object::all() as $object) {
-												echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-											}
-											?>
-										</select>
-									</div>
-                  <div class="col-lg-4"/>
-								</div>
-								<div class="form-group">
-									<label class="col-lg-4 control-label">{{Identifiant Compteur}} :</label>
-									<div class="col-lg-4">
-										<input type="text" class="eqLogicAttr form-control tooltips" title="{{Identifiant du compteur aussi connu sous le nom ADCO.}}" data-l1key="logicalId" placeholder="{{ADCO du compteur}}"/>
-									</div>
-                  <div class="col-lg-4"/>
-								</div>
-								<div class="form-group" style="display:none">
-									<label class="col-lg-4 control-label">{{Catégorie}} :</label>
-									<div class="col-lg-8">
-										<!--<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="energy" checked/>-->
-										<?php
-										/*foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
-											echo '<label class="checkbox-inline">';
-											echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
-											echo '</label>';
-										}*/
-										?>
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="col-sm-4 control-label">{{Etat de l'objet}} :</label>
-									<div class="col-sm-8">
-										<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-										<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-									</div>
-								</div>
-							</fieldset>
-						</form>
-					</div>
-					<div class="col-lg-6">
-						<form class="form-horizontal">
-							<fieldset>
-								<!--<legend>{{Paramètres}}</legend>-->
-								<div class="form-group">
-									<label class="col-md-3 control-label pull-left">{{Votre abonnement :}}</label>
-									<div class="col-md-4">
-										<select class="eqLogicAttr form-control tooltips" title="{{Abonnement présent sur le compteur}}" data-l1key="configuration" data-l2key="abonnement">
-										<option value="">Aucun</option>
-										<option value="base">Base (HP)</option>
-										<option value="basetri">Base triphasé</option>
-										<option value="bleu">Bleu (HP/HC)</option>
-										<option value="bleutri">Bleu triphasé</option>
-										<option value="tempo">Tempo / EJP</option>
-										<option value="tempotri">Tempo triphasé</option>
-										</select>
-									</div>
-                  <div class="col-lg-3"/>
-								</div>
-                <div class="form-group">
-									<label class="col-md-3 control-label pull-left">{{Création des commandes}} <sup><i class="fa fa-question-circle tooltips" title="{{Créer automatiquement les commandes envoyées par le compteur}}" style="font-size : 1em;color:grey;"></i></sup></label>
-									<div class="col-md-7 tooltips">
-										<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="AutoCreateFromCompteur" checked/>{{Automatique}}</label>
-									</div>
-									<!--<div class="col-md-2">
-										<input class="eqLogicAttr" style="display:none" type="checkbox"  data-l1key="configuration" data-l2key="AutoGenerateFields" id="checkbox-autocreate"/>
-										<a class="btn btn-info btn-sm eqLogicAction tooltips"  id="create_data_teleinfo" title="{{Permet de créer automatiquement les commandes nécessaires.}}" id="createcmd"><i class="fa fa-plus-circle"></i> {{Créer}}</a><br/><br/>
-									</div>-->
-								</div>
-								<div class="form-group">
-                  <label class="col-md-3 control-label pull-left">{{Vérifier}} <sup><i class="fa fa-question-circle tooltips" title="{{Obtenir des informations sur la réception des données}}" style="font-size : 1em;color:grey;"></i></sup></label>
-									<div class="col-md-3">
-										<a class="btn btn-info tooltips"  id="bt_teleinfoHealth"><i class="fa fa-medkit"></i>{{ Santé}}</a>
-									</div>
-									<!--<div class="col-md-2">
-										<a class="btn btn-info btn-sm eqLogicAction tooltips"  data-action="save" title="{{Applique le template}}"><i class="fa fa-plus-circle"></i> {{Appliquer}}</a><br/><br/>
-									</div>-->
-								</div>
-                <div class="form-group">
-									<div class="col-lg-12">
-                    <div class="alert alert-info globalRemark" style="">{{Attention, il est nécessaire d'activer l'historisation des index pour utiliser les statistiques}}</div>
-									</div>
-								</div>
-							</fieldset>
-						</form>
-					</div>
-				</div>
-			</div>
-			<div role="tabpanel" class="tab-pane" id="commandtab">
-      </br>
-					<a class="btn btn-success btn-sm cmdAction" id="addDataToTable"><i class="fa fa-plus-circle"></i> {{Ajouter une donnée}}</a> &nbsp;
-					<a class="btn btn-success btn-sm cmdAction expertModeVisible" id="addStatToTable"><i class="fa fa-plus-circle"></i> {{Ajouter une statistique}}</a><br/><br/>
-
-				<table id="table_cmd" class="table table-bordered table-condensed">
-					<thead>
-						<tr>
-							<th style="width: 50px;">#</th>
-							<th style="width: 150px;">{{Nom}}</th>
-							<th style="width: 110px;">{{Sous-Type}}</th>
-							<th style="width: 200px;">{{Donnée}}</th>
-							<th style="width: 150px;">{{Paramètres}}</th>
-							<th style="width: 150px;"></th>
-						</tr>
-					</thead>
-					<tbody>
-
-					</tbody>
-				</table>
-
-				<form class="form-horizontal">
-					<fieldset>
-						<div class="form-actions">
-
-						</div>
-					</fieldset>
-				</form>
-			</div>
+      <div class="row">
+          <div class="col-lg-6">
+              <form class="form-horizontal">
+                  <fieldset>
+                      <div class="form-group">
+                          <label class="col-lg-4 control-label">{{Nom de l'équipement}} :</label>
+                          <div class="col-lg-4">
+                              <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                              <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}"/>
+                          </div>
+                          <div class="col-lg-4">
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label class="col-lg-4 control-label" >{{Objet parent :}}</label>
+                          <div class="col-lg-4">
+                              <select class="eqLogicAttr form-control" data-l1key="object_id">
+                                  <option value="">{{Aucun}}</option>
+                                  <?php
+                                  foreach (object::all() as $object) {
+                                      echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                                  }
+                                  ?>
+                              </select>
+                          </div>
+                          <div class="col-lg-4">
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label class="col-lg-4 control-label">{{Identifiant Compteur}} :</label>
+                          <div class="col-lg-4">
+                              <input type="text" class="eqLogicAttr form-control tooltips" title="{{Identifiant du compteur aussi connu sous le nom ADCO.}}" data-l1key="logicalId" placeholder="{{ADCO du compteur}}"/>
+                          </div>
+                          <div class="col-lg-4">
+                          </div>
+                      </div>
+                      <div class="form-group" style="display:none">
+                          <label class="col-lg-4 control-label">{{Catégorie}} :</label>
+                          <div class="col-lg-8">
+                              <!--<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="energy" checked/>-->
+                              <?php
+                              /*foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+                              echo '<label class="checkbox-inline">';
+                              echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+                              echo '</label>';
+                          }*/
+                              ?>
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label class="col-lg-4 control-label">{{Etat de l'objet}} :</label>
+                          <div class="col-lg-8">
+                              <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+                              <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+                          </div>
+                      </div>
+                  </fieldset>
+              </form>
+          </div>
+          <div class="col-lg-6">
+              <form class="form-horizontal">
+                  <fieldset>
+                      <!--<legend>{{Paramètres}}</legend>-->
+                      <div class="form-group">
+                          <label class="col-lg-3 control-label pull-left">{{Votre abonnement :}}</label>
+                          <div class="col-lg-4">
+                              <select class="eqLogicAttr form-control tooltips" title="{{Abonnement présent sur le compteur}}" data-l1key="configuration" data-l2key="abonnement">
+                                  <option value="">Aucun</option>
+                                  <option value="base">Base (HP)</option>
+                                  <option value="basetri">Base triphasé</option>
+                                  <option value="bleu">Bleu (HP/HC)</option>
+                                  <option value="bleutri">Bleu triphasé</option>
+                                  <option value="tempo">Tempo / EJP</option>
+                                  <option value="tempotri">Tempo triphasé</option>
+                              </select>
+                          </div>
+                          <div class="col-lg-5">
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label class="col-lg-3 control-label pull-left">{{Création des commandes}} <sup><i class="fa fa-question-circle tooltips" title="{{Créer automatiquement les commandes envoyées par le compteur}}" style="font-size : 1em;color:grey;"></i></sup></label>
+                          <div class="col-lg-7 tooltips">
+                              <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="AutoCreateFromCompteur" checked/>{{Automatique}}</label>
+                          </div>
+                          <div class="col-lg-2">
+                          </div>
+                          <!--<div class="col-md-2">
+                          <input class="eqLogicAttr" style="display:none" type="checkbox"  data-l1key="configuration" data-l2key="AutoGenerateFields" id="checkbox-autocreate"/>
+                          <a class="btn btn-info btn-sm eqLogicAction tooltips"  id="create_data_teleinfo" title="{{Permet de créer automatiquement les commandes nécessaires.}}" id="createcmd"><i class="fa fa-plus-circle"></i> {{Créer}}</a><br/><br/>
+                          </div>-->
+                      </div>
+                      <div class="form-group">
+                          <label class="col-lg-3 control-label pull-left">{{Vérifier}} <sup><i class="fa fa-question-circle tooltips" title="{{Obtenir des informations sur la réception des données}}" style="font-size : 1em;color:grey;"></i></sup></label>
+                          <div class="col-lg-3">
+                              <a class="btn btn-info tooltips"  id="bt_teleinfoHealth"><i class="fa fa-medkit"></i>{{ Santé}}</a>
+                          </div>
+                          <div class="col-lg-6">
+                          </div>
+                          <!--<div class="col-md-2">
+                          <a class="btn btn-info btn-sm eqLogicAction tooltips"  data-action="save" title="{{Applique le template}}"><i class="fa fa-plus-circle"></i> {{Appliquer}}</a><br/><br/>
+                          </div>-->
+                      </div>
+                      <div class="form-group">
+                          <div class="col-lg-12">
+                              <div class="alert alert-info globalRemark" style="">{{Attention, il est nécessaire d'activer l'historisation des index pour utiliser les statistiques}}</div>
+                          </div>
+                      </div>
+                  </fieldset>
+              </form>
+          </div>
+      </div>
+  </div>
+  <div role="tabpanel" class="tab-pane" id="commandtab">
+  </br>
+  <a class="btn btn-success btn-sm cmdAction" id="addDataToTable"><i class="fa fa-plus-circle"></i> {{Ajouter une donnée}}</a> &nbsp;
+  <a class="btn btn-success btn-sm cmdAction expertModeVisible" id="addStatToTable"><i class="fa fa-plus-circle"></i> {{Ajouter une statistique}}</a><br/><br/>
+  <table id="table_cmd" class="table table-bordered table-condensed">
+      <thead>
+          <tr>
+              <th style="width: 50px;">#</th>
+              <th style="width: 150px;">{{Nom}}</th>
+              <th style="width: 110px;">{{Sous-Type}}</th>
+              <th style="width: 200px;">{{Donnée}}</th>
+              <th style="width: 150px;">{{Paramètres}}</th>
+              <th style="width: 150px;"></th>
+          </tr>
+      </thead>
+      <tbody>
+      </tbody>
+  </table>
+  <form class="form-horizontal">
+      <fieldset>
+          <div class="form-actions">
+          </div>
+      </fieldset>
+  </form>
+</div>
 		</div>
 	</div>
 </div>
