1f760d2cccb9f76f51f481e9815b2db8adfe30bc
ColonelMoutarde <4697568+ColonelMoutarde@users.noreply.github.com>
2018-02-28 19:04:20 +0100

    remove ?> + PSR-2 indentation
    strict comparaison

--- core/ajax/teleinfo.ajax.php
+++ core/ajax/teleinfo.ajax.php
@@ -21,7 +21,7 @@ try {
     include_file('core', 'authentification', 'php');
 
     if (!isConnect('admin')) {
-        throw new Exception('401 Unauthorized');
+        throw new \Exception('401 Unauthorized');
     }
 
     if (init('action') == 'stopDeamon') {
@@ -29,47 +29,47 @@ try {
         ajax::success();
     }
 
-	if (init('action') == 'restartDeamon') {
-		$port = config::byKey('port', 'teleinfo', 'none');
-		$_2cpt_cartelectronic = config::byKey('2cpt_cartelectronic', 'teleinfo');
-		if($_2cpt_cartelectronic == 1){
+    if (init('action') == 'restartDeamon') {
+        $port                 = config::byKey('port', 'teleinfo', 'none');
+        $_2cpt_cartelectronic = config::byKey('2cpt_cartelectronic', 'teleinfo');
+        if ($_2cpt_cartelectronic == 1) {
+            
+        }
+        if ($port == 'none') {
+            ajax::success();
+        }
+        teleinfo::stopDeamon();
+        if (teleinfo::deamonRunning()) {
+            throw new \Exception(__('Impossible d\'arrêter le démon', __FILE__));
+        }
+        log::clear('teleinfocmd');
+        teleinfo::cron();
+        ajax::success();
+    }
 
-		}
-		if ($port == 'none') {
-			ajax::success();
-		}
-		teleinfo::stopDeamon();
-		if (teleinfo::deamonRunning()) {
-			throw new Exception(__('Impossible d\'arr�ter le d�mon', __FILE__));
-		}
-		log::clear('teleinfocmd');
-		teleinfo::cron();
-		ajax::success();
-	}
+    if (init('action') == 'restartSlaveDeamon') {
+        if (config::byKey('jeeNetwork::mode') == 'master') {
+            $jeeNetwork = jeeNetwork::byId(init('id'));
+            if (!is_object($jeeNetwork)) {
+                throw new \Exception(__('Impossible de trouver l\'esclave : ', __FILE__) . init('id'));
+            }
+            $jeeNetwork->sendRawRequest('restartDeamon', array('plugin' => 'teleinfo'));
+        }
+        ajax::success();
+    }
 
-	if (init('action') == 'restartSlaveDeamon') {
-		if (config::byKey('jeeNetwork::mode') == 'master') {
-			$jeeNetwork = jeeNetwork::byId(init('id'));
-			if (!is_object($jeeNetwork)) {
-				throw new Exception(__('Impossible de trouver l\'esclave : ', __FILE__) . init('id'));
-			}
-			$jeeNetwork->sendRawRequest('restartDeamon', array('plugin' => 'teleinfo'));
-		}
-		ajax::success();
-	}
+    if (init('action') == 'stopSlaveDeamon') {
+        if (config::byKey('jeeNetwork::mode') == 'master') {
+            $jeeNetwork = jeeNetwork::byId(init('id'));
+            if (!is_object($jeeNetwork)) {
+                throw new \Exception(__('Impossible de trouver l\'esclave : ', __FILE__) . init('id'));
+            }
+            $jeeNetwork->sendRawRequest('stopDeamon', array('plugin' => 'teleinfo'));
+        }
+        ajax::success();
+    }
 
-	if (init('action') == 'stopSlaveDeamon') {
-		if (config::byKey('jeeNetwork::mode') == 'master') {
-			$jeeNetwork = jeeNetwork::byId(init('id'));
-			if (!is_object($jeeNetwork)) {
-				throw new Exception(__('Impossible de trouver l\'esclave : ', __FILE__) . init('id'));
-			}
-			$jeeNetwork->sendRawRequest('stopDeamon', array('plugin' => 'teleinfo'));
-		}
-		ajax::success();
-	}
-
-	if (init('action') == 'getTeleinfo') {
+    if (init('action') == 'getTeleinfo') {
         if (init('object_id') == '') {
             $_GET['object_id'] = $_SESSION['user']->getOptions('defaultDashboardObject');
         }
@@ -78,13 +78,13 @@ try {
             $object = object::rootObject();
         }
         if (!is_object($object)) {
-            throw new Exception('{{Aucun objet racine trouv�}}');
+            throw new \Exception('{{Aucun objet racine trouv�}}');
         }
         $return = array('object' => utils::o2a($object));
 
         $date = array(
             'start' => init('dateStart'),
-            'end' => init('dateEnd'),
+            'end'   => init('dateEnd'),
         );
 
         if ($date['start'] == '') {
@@ -102,126 +102,125 @@ try {
         ajax::success($return);
     }
 
-	if (init('action') == 'getInformation') {
-		if(init('eqLogic_id') != null){
-			$eqLogic = eqLogic::byId(init('eqLogic_id'));
-				$return[$eqLogic->getId()] = utils::o2a($eqLogic);
-				$return[$eqLogic->getId()]['cmd'] = array();
-				foreach ($eqLogic->getCmd() as $cmd) {
-					$cmd_info = utils::o2a($cmd);
-					$cmd_info['value'] = $cmd->execCmd(null, 2);
-					$return[$eqLogic->getId()]['cmd'][] = $cmd_info;
-				}
-			ajax::success($return);
-		}else{
-			$eqLogics = eqLogic::byType('teleinfo');
-			foreach ($eqLogics as $eqLogic) {
-				$return[$eqLogic->getId()] = utils::o2a($eqLogic);
-				$return[$eqLogic->getId()]['cmd'] = array();
-				foreach ($eqLogic->getCmd() as $cmd) {
-					$cmd_info = utils::o2a($cmd);
-					$cmd_info['value'] = $cmd->execCmd(null, 2);
-					$return[$eqLogic->getId()]['cmd'][] = $cmd_info;
-				}
-			}
-			ajax::success($return);
-		}
+    if (init('action') == 'getInformation') {
+        if (init('eqLogic_id') !== null) {
+            $eqLogic                          = eqLogic::byId(init('eqLogic_id'));
+            $return[$eqLogic->getId()]        = utils::o2a($eqLogic);
+            $return[$eqLogic->getId()]['cmd'] = array();
+            foreach ($eqLogic->getCmd() as $cmd) {
+                $cmd_info                           = utils::o2a($cmd);
+                $cmd_info['value']                  = $cmd->execCmd(null, 2);
+                $return[$eqLogic->getId()]['cmd'][] = $cmd_info;
+            }
+            ajax::success($return);
+        } else {
+            $eqLogics = eqLogic::byType('teleinfo');
+            foreach ($eqLogics as $eqLogic) {
+                $return[$eqLogic->getId()]        = utils::o2a($eqLogic);
+                $return[$eqLogic->getId()]['cmd'] = array();
+                foreach ($eqLogic->getCmd() as $cmd) {
+                    $cmd_info                           = utils::o2a($cmd);
+                    $cmd_info['value']                  = $cmd->execCmd(null, 2);
+                    $return[$eqLogic->getId()]['cmd'][] = $cmd_info;
+                }
+            }
+            ajax::success($return);
+        }
     }
-	if (init('action') == 'getHealth') {
-		//$eqLogics = eqLogic::byType('teleinfo');
-		if(init('eqLogicID') != null){
-			$teleinfo = teleinfo::byLogicalId(init('eqLogicID'), 'teleinfo');
-			$health_cmd = $teleinfo->getCmd('info','health');
-			//$return[0] = $health_cmd;
-			$return = array('object' => utils::o2a($health_cmd));
-			$return["ADCO"] = init('eqLogicID');
-			ajax::success($return);
-		}
-		else{
-			$teleinfo = teleinfo::byType('teleinfo');
-			foreach ($teleinfo as $eqLogic) {
-				$health_cmd = $eqLogic->getCmd('info','health');
-				//$return[0] = $health_cmd;
-				$return = array('object' => utils::o2a($health_cmd));
-				$return["ADCO"] = $eqLogic->getLogicalId();
-				ajax::success($return);
-			}
-		}
+    if (init('action') == 'getHealth') {
+        //$eqLogics = eqLogic::byType('teleinfo');
+        if (init('eqLogicID') !== null) {
+            $teleinfo       = teleinfo::byLogicalId(init('eqLogicID'), 'teleinfo');
+            $health_cmd     = $teleinfo->getCmd('info', 'health');
+            //$return[0] = $health_cmd;
+            $return         = array('object' => utils::o2a($health_cmd));
+            $return["ADCO"] = init('eqLogicID');
+            ajax::success($return);
+        } else {
+            $teleinfo = teleinfo::byType('teleinfo');
+            foreach ($teleinfo as $eqLogic) {
+                $health_cmd     = $eqLogic->getCmd('info', 'health');
+                //$return[0] = $health_cmd;
+                $return         = array('object' => utils::o2a($health_cmd));
+                $return["ADCO"] = $eqLogic->getLogicalId();
+                ajax::success($return);
+            }
+        }
 
-		ajax::error("", "");
-	}
-	if (init('action') == 'getInfoDaemon') {
+        ajax::error("", "");
+    }
+    if (init('action') == 'getInfoDaemon') {
 
-		$return = array();
-		$_nbLines = 1000;
-		$replace = array(
-			'&gt;' => '>',
-			'&apos;' => '',
-		);
-		$page = array();
+        $return   = array();
+        $_nbLines = 1000;
+        $replace  = array(
+            '&gt;'   => '>',
+            '&apos;' => '',
+        );
+        $page     = array();
 
-		$path = realpath(dirname(__FILE__) . '/../../ressources/teleinfo.log');
-		//log::add('teleinfo', 'info', $path);
-		if (!file_exists($path)) {
-			$return['result'] = array('Deamon non lancé');
-			ajax::success($return);
-		}else{
-			$log = new SplFileObject($path);
-			if ($log) {
-				$log->seek(0); //Seek to the begening of lines
-				$linesRead = 0;
-				while ($log->valid() && $linesRead != $_nbLines) {
-					$line = trim($log->current()); //get current line
-					if ($line != '') {
-						array_unshift($page, $line);
-					}
-					$log->next(); //go to next line
-					$linesRead++;
-				}
-			}
-			//return $page;
-			$return['result'] = $page;
-			ajax::success($return);
-		}
-	}
+        $path = realpath(dirname(__FILE__) . '/../../ressources/teleinfo.log');
+        //log::add('teleinfo', 'info', $path);
+        if (!file_exists($path)) {
+            $return['result'] = array('Deamon non lancé');
+            ajax::success($return);
+        } else {
+            $log = new SplFileObject($path);
+            if ($log) {
+                $log->seek(0); //Seek to the begening of lines
+                $linesRead = 0;
+                while ($log->valid() && $linesRead != $_nbLines) {
+                    $line = trim($log->current()); //get current line
+                    if ($line != '') {
+                        array_unshift($page, $line);
+                    }
+                    $log->next(); //go to next line
+                    $linesRead++;
+                }
+            }
+            //return $page;
+            $return['result'] = $page;
+            ajax::success($return);
+        }
+    }
 
-	if (init('action') == 'getInfoExternalDaemon') {
-		$jeeNetwork = jeeNetwork::byPlugin('enocean');
+    if (init('action') == 'getInfoExternalDaemon') {
+        $jeeNetwork = jeeNetwork::byPlugin('enocean');
 
-		$return['result'] = $jeeNetwork;
-		ajax::success($return);
-		//$jeeNetwork = jeeNetwork::byId(init('id'));
-		//	//if (!is_object($jeeNetwork)) {
-		//		throw new Exception(__('Impossible de trouver l\'esclave : ', __FILE__) . init('id'));
-		//	}
-		//	$jeeNetwork->sendRawRequest('getInfoDaemon', array('plugin' => 'teleinfo'));
-	}
-	if (init('action') == 'getHistory') {
-		$return = array();
-		/*$data = array();
-		$datetime = null;*/
-		//console.log("essai");
-		//console.log('Commande : ' . init('id'));
-		$return = history::byCmdIdDatetime( init('id'), date('Y-m-d H:i:s'));
-		//$dateEnd = date('Y-m-d H:i:s');
+        $return['result'] = $jeeNetwork;
+        ajax::success($return);
+        //$jeeNetwork = jeeNetwork::byId(init('id'));
+        //	//if (!is_object($jeeNetwork)) {
+        //		throw new Exception(__('Impossible de trouver l\'esclave : ', __FILE__) . init('id'));
+        //	}
+        //	$jeeNetwork->sendRawRequest('getInfoDaemon', array('plugin' => 'teleinfo'));
+    }
+    if (init('action') == 'getHistory') {
+        $return = array();
+        /* $data = array();
+          $datetime = null; */
+        //console.log("essai");
+        //console.log('Commande : ' . init('id'));
+        $return = history::byCmdIdDatetime(init('id'), date('Y-m-d H:i:s'));
+        //$dateEnd = date('Y-m-d H:i:s');
         ajax::success($return);
     }
 
-	if (init('action') == 'getCout') {
-		$return = array();
-		/*$data = array();
-		$datetime = null;*/
-		//console.log("essai");
-		//console.log('Commande : ' . init('id'));
-		$return = history::byCmdIdDatetime( init('id'), date('Y-m-d H:i:s'));
-		//$dateEnd = date('Y-m-d H:i:s');
+    if (init('action') == 'getCout') {
+        $return = array();
+        /* $data = array();
+          $datetime = null; */
+        //console.log("essai");
+        //console.log('Commande : ' . init('id'));
+        $return = history::byCmdIdDatetime(init('id'), date('Y-m-d H:i:s'));
+        //$dateEnd = date('Y-m-d H:i:s');
         ajax::success($return);
     }
 
 
-    throw new Exception('Aucune methode correspondante');
+    throw new \Exception('Aucune methode correspondante');
     /*     * *********Catch exeption*************** */
-} catch (Exception $e) {
+} catch (\Exception $e) {
     ajax::error(displayExeption($e), $e->getCode());
 }
-?>
+ 
