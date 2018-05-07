14ff573a5387042b94677e4a7c2ba5d603395ad5
Cédric <ticed35@hotmail.com>
2018-05-07 11:52:13 -0400

    Travis links

--- travis/tools/lintAllPythonFiles.sh
+++ travis/tools/lintAllPythonFiles.sh
@@ -2,6 +2,6 @@
 for file in `find . -name "*.py" ! -name "__init__.py"`;
   do
       echo "Check $file with pylint"
-      python -m pylint --rcfile=tests/tools/.pylintrc $file
+      python -m pylint --rcfile=travis/tools/.pylintrc $file
   done
   
--- travis/tools/spellCheckMD.sh
+++ travis/tools/spellCheckMD.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-for file in *.md docs/fr_FR/*.md; 
-  do 
+for file in *.md docs/fr_FR/*.md;
+  do
   echo $files
   if [ $file = "docs/fr_FR/index-template.md" ] || [ $file = "docs/fr_FR/index.md" ]
   then
     echo "skip "$file
   else
     echo "process "$file
-    cat $file | aspell --personal=./tests/tools/.aspell.fr.pws --lang=fr --encoding=utf-8 list;
+    cat $file | aspell --personal=./travis/tools/.aspell.fr.pws --lang=fr --encoding=utf-8 list;
   fi
-done 
+done
