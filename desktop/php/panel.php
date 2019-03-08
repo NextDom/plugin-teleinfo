25cb83c49977fd4e0eb1ec0eb4688e166555a60c
Cédric <ticed35@hotmail.com>
2019-03-08 12:31:06 -0500

    New Panel

--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -12,116 +12,125 @@ $eqLogics = eqLogic::byType('teleinfo');
 
 ?>
 
-<div class="row row-overflow" id="div_teleinfo">
+<div class="row" id="div_teleinfo">
     <div class="row">
-        <div class="col-lg-12">
-        <input id="in_startDate" class="form-control input-sm in_datepicker" style="display : inline-block; width: 150px;" value="<?php echo $date['start']?>"/>
-        <input id="in_endDate" class="form-control input-sm in_datepicker" style="display : inline-block; width: 150px;" value="<?php echo $date['end']?>"/>
-        <a class="btn btn-success btn-sm tooltips" id='bt_validChangeDate' title="{{Attention une trop grande plage de dates peut mettre très longtemps à être calculée ou même ne pas s'afficher}}">{{Ok}}</a>
-        <select id="eqlogic_select">
-						<?php
-						foreach ($eqLogics as $eqLogic) {
-							echo '<option value="' . $eqLogic->getId() . '">"' . $eqLogic->getHumanName(true) . '"</option>';
-						}
-						?>
-					</select>
+        <div class="col-lg-8 col-lg-offset-2" style="height: 360px;padding-top:10px;">
 
-        <a class="btn btn-default pull-right tooltips" style="margin-right:15px"  id="bt_teleinfoPanelSante" title="{{Vérifier l'intégrité des données}}"><i class="fa fa-check-circle-o"></i>{{ Vérifier }}</a>
-        </div>
-        <div class="col-lg-8 col-lg-offset-2" style="height: 300px;">
 			<form class="form-horizontal">
-				<fieldset>
-					<legend>{{Ma consommation}} 
-					</legend>
-					<div class="form-group col-md-4" style="top:-10px;left:25px;">
-                        <div class="">
-						<label class="control-label" style="font-size: 1em;">{{Journée}}</label>
-						</div>
-						<div class="">
-							<span class='' style="font-size: 1.3em;">
-								<span class="teleinfoAttr" data-l1key="conso" data-l2key="day"></span>
-								kWh
-							</span>
-						</div>
-					</div>
-					<div class="form-group col-md-4" style="top:-10px">
-						<div class="">
-    						<label class="control-label" style="font-size: 1em;">{{Mois}}</label>
-							<div>
-							<span class='' style="font-size: 1.3em;">
-								<span class="teleinfoAttr" data-l1key="conso" data-l2key="month"></span>
-								kWh
-							</span>
-							</div>
-						</div>
-						<div class="">
-							<label class="control-label" style="font-size: 1em;">{{Mois N-1}} <sup><i class="fa fa-question-circle tooltips" title="{{Du 1er Janvier au même jour au même jour qu'aujourd'hui}}" style="font-size : 1em;color:grey;"></i></sup></label>
-							<div>
-							<span class='' style="font-size: 1.3em;">
-								<span class="teleinfoAttr" data-l1key="conso" data-l2key="month"></span>
-								kWh
-							</span>
-							</div>
-						</div>
-					</div>
-					<div class="form-group col-md-4" style="top:-10px">
-						<div class="">
+				<fieldset style="border: 1px solid #e5e5e5; border-radius: 5px 5px 0px 5px;">
+					<div style="padding-top:10px;padding-left:24px;padding-bottom:25px;color: #333;font-size: 1.5em;">Ma consommation
+                        <select id="eqlogic_select" style="color: #555;font-size: 16px;border-radius: 3px;border:1px solid #ccc;">
+                						<?php
+                						foreach ($eqLogics as $eqLogic) {
+                							echo '<option value="' . $eqLogic->getId() . '">"' . $eqLogic->getHumanName(true) . '"</option>';
+                						}
+                						?>
+                		</select>
+
+                        <a class="btn btn-default btn-sm pull-right tooltips" style="margin-right:10px;" id="bt_teleinfoPanelSante" title="Vérifier l'intégrité des données"><i class="fa fa-check-circle-o"></i> Vérifier </a>
+                        <a style="margin-right:5px;" class="pull-right btn btn-success btn-sm tooltips" id='bt_validChangeDate' title="{{Attention une trop grande plage de dates peut mettre très longtemps à être calculée ou même ne pas s'afficher}}">{{Ok}}</a>
+                        <input id="in_endDate" class="pull-right form-control input-sm in_datepicker" style="display : inline-block; width: 100px;" value="<?php echo $date['end']?>"/>
+                        <input id="in_startDate" class="pull-right form-control input-sm in_datepicker" style="display : inline-block; width: 100px;" value="<?php echo $date['start']?>"/>
+
+
+                    </div>
+
+    					<div class="form-group col-md-4" style="left:25px;">
                             <div class="">
-    						<label class="control-label" style="font-size: 1em;">{{Année}}</label>
+    						<label class="control-label" style="font-size: 1em;">{{Journée}}</label>
     						</div>
-							<span class='' style="font-size: 1.3em;">
-								<span class="teleinfoAttr" data-l1key="conso" data-l2key="monthlastyear"></span>
-								kWh
-							</span>
-						</div>
-						<div class="">
-                            <div class="">
-    						<label class="control-label" style="font-size: 1em;">{{Année N-1}} <sup><i class="fa fa-question-circle tooltips" title="{{Du 1er Janvier au même jour au même jour qu'aujourd'hui}}" style="font-size : 1em;color:grey;"></i></sup></label>
+    						<div class="">
+    							<span class='' style="font-size: 1.3em;">
+    								<span class="teleinfoAttr" data-l1key="conso" data-l2key="day"></span>
+    								kWh
+    							</span>
     						</div>
-							<span class='' style="font-size: 1.3em;">
-								<span class="teleinfoAttr" data-l1key="conso" data-l2key="yearlastyear"></span>
-								kWh
-							</span>
-						</div>
-					</div>
-					<?php
-					switch (date("m")) {
-						case "12":
-						case "01":
-						case "02":
-							echo '<img class="pull-right" src="plugins/teleinfo/ressources/panel/portraitconso_saisons_energie_hiver.png" height="105" width="600" />';
-						break;
-						case "03":
-						case "04":
-						case "05":
-							echo '<img class="pull-right" src="plugins/teleinfo/ressources/panel/portraitconso_saisons_energie_printemps.png" height="105" width="600" />';
-						break;
-						case "06":
-						case "07":
-						case "08":
-							echo '<img class="pull-right" src="plugins/teleinfo/ressources/panel/portraitconso_saisons_energie_ete.png" height="105" width="600" />';
-						break;
-						case "09":
-						case "10":
-						case "11":
-							echo '<img class="pull-right" src="plugins/teleinfo/ressources/panel/portraitconso_saisons_energie_automne.png" height="105" width="600" />';
-						break;
-					}
-					
-					?>
+    					</div>
+    					<div class="form-group col-md-4" style="">
+    						<div class="">
+        						<label class="control-label" style="font-size: 1em;">{{Mois}}</label>
+    							<div>
+    							<span class='' style="font-size: 1.3em;">
+    								<span class="teleinfoAttr" data-l1key="conso" data-l2key="month"></span>
+    								kWh
+    							</span>
+    							</div>
+    						</div>
+    						<div class="">
+    							<label class="control-label" style="font-size: 1em;">{{Mois N-1}} <sup><i class="fa fa-question-circle tooltips" title="{{Du 1er Janvier au même jour au même jour qu'aujourd'hui}}" style="font-size : 1em;color:grey;"></i></sup></label>
+    							<div>
+    							<span class='' style="font-size: 1.3em;">
+    								<span class="teleinfoAttr" data-l1key="conso" data-l2key="monthlastyear"></span>
+    								kWh
+    							</span>
+    							</div>
+    						</div>
+    					</div>
+    					<div class="form-group col-md-4" style="">
+    						<div class="">
+                                <div class="">
+        						<label class="control-label" style="font-size: 1em;">{{Année}}</label>
+        						</div>
+    							<span class='' style="font-size: 1.3em;">
+    								<span class="teleinfoAttr" data-l1key="conso" data-l2key="year"></span>
+    								kWh
+    							</span>
+    						</div>
+    						<div class="">
+                                <div class="">
+        						<label class="control-label" style="font-size: 1em;">{{Année N-1}} <sup><i class="fa fa-question-circle tooltips" title="{{Du 1er Janvier au même jour au même jour qu'aujourd'hui}}" style="font-size : 1em;color:grey;"></i></sup></label>
+        						</div>
+    							<span class='' style="font-size: 1.3em;">
+    								<span class="teleinfoAttr" data-l1key="conso" data-l2key="yearlastyear"></span>
+    								kWh
+    							</span>
+    						</div>
+    					</div>
+
+
+    					<?php
+    					switch (date("m")) {
+    						case "12":
+    						case "01":
+    						case "02":
+    							echo '<img class="pull-right" src="plugins/teleinfo/ressources/panel/portraitconso_saisons_energie_hiver.png" height="105" width="600" />';
+    						break;
+    						case "03":
+    						case "04":
+    						case "05":
+    							echo '<img class="pull-right" src="plugins/teleinfo/ressources/panel/portraitconso_saisons_energie_printemps.png" height="105" width="600" />';
+    						break;
+    						case "06":
+    						case "07":
+    						case "08":
+    							echo '<img class="pull-right" src="plugins/teleinfo/ressources/panel/portraitconso_saisons_energie_ete.png" height="105" width="600" />';
+    						break;
+    						case "09":
+    						case "10":
+    						case "11":
+    							echo '<img class="pull-right" src="plugins/teleinfo/ressources/panel/portraitconso_saisons_energie_automne.png" height="105" width="600" />';
+    						break;
+    					}
+
+    					?>
+
 				</fieldset>
 			</form>
         </div>
 		<div class="col-lg-2">
 		</div>
-		<div class="col-lg-6">
-			<legend>Evolution de la consommation journalière</legend>
-			<div id='div_graphGlobalJournalier'></div>
-		</div>
-		<div class="col-lg-6">
-			<legend>Evolution de la consommation mensuelle</legend>
-			<div id='div_graphGlobalIndex'></div>
-		</div>		
+        </div>
+        <div class="row" style="padding-left:5px;padding-right:5px;">
+    		<div class="col-lg-6">
+    			<legend>Evolution de la consommation journalière</legend>
+    			<div id='div_graphGlobalJournalier'></div>
+    		</div>
+    		<div class="col-lg-6">
+    			<legend>Evolution de la consommation mensuelle</legend>
+    			<div id='div_graphGlobalIndex'></div>
+    		</div>
+        </div>
+        <div class="row" style="padding-left:5px;padding-right:5px;">
 		<div class="col-lg-6">
 			<legend>Puissance</legend>
 			<div id='div_graphGlobalPower'></div>
