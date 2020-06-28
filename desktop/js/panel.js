bfbebe3a1bb91553d8e870c06e179638d2077cb3
Cédric <ticed35@hotmail.com>
2020-06-28 13:39:51 -0400

    Update panel.js

--- desktop/js/panel.js
+++ desktop/js/panel.js
@@ -487,9 +487,15 @@ function getCommandHistoryCout(div, type, cout, object) {
 							console.log("[getCommandHistoryCout] " + type + " " + cout + " : " + myCommandHistory.data[myCommandHistory.data.length - 1][1] / 1000 + " * " + valeurCout)
 
 						}else {
-							myCommandHistory.data.splice(-1,1);
+                          if(myCommandHistory.data.length == 1){
+                            tableCouts[type] = tableCouts[type] + (myCommandHistory.maxValue / 1000) * valeurCout;
+                            console.log("[getCommandHistoryCout] " + type + " " + cout + " : " + (myCommandHistory.maxValue / 1000) * valeurCout);
+                          }
+                          else {
+                            myCommandHistory.data.splice(-1,1);
 							tableCouts[type] = tableCouts[type] + (myCommandHistory.data.reduce(function(prev, cur) {  return prev + cur[1];}, 0) / 1000) * valeurCout;
 							console.log("[getCommandHistoryCout] " + type + " " + cout + " : " + myCommandHistory.data.reduce(function(prev, cur) {  return prev + cur[1];}, 0) / 1000 + " * " + valeurCout)
+                          }
 						}
 
                         div.text(' (~' + tableCouts[type].toFixed(3) + ' €)');
