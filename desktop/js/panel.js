62cac93f637af7e2add90687c27177be850cbc14
Cédric <ticed35@hotmail.com>
2020-06-28 13:29:13 -0400

    Update panel.js

--- desktop/js/panel.js
+++ desktop/js/panel.js
@@ -417,10 +417,14 @@ function getCommandHistoryValue(div, type , object) {
         error: function (error) {
         },
         success: function (myCommandHistory) {
-            //console.log(myCommandHistory);
-            myCommandHistory.data.splice(-1,1);
-            div.text(myCommandHistory.data.reduce(function(prev, cur) {  return prev + cur[1];}, 0) / 1000);
-            console.log("[getCommandHistoryValue] " + type + " | from : " + from + " | to : " + to + " | value : " + myCommandHistory.data.reduce(function(prev, cur) {  return prev + cur[1];}, 0) / 1000);
+            if(myCommandHistory.data.length == 1){
+              div.text(myCommandHistory.maxValue / 1000)
+              console.log("[getCommandHistoryValue] " + type + " | from : " + from + " | to : " + to + " | value : " + myCommandHistory.maxValue / 1000);
+            }else {
+              myCommandHistory.data.splice(-1,1);
+              div.text(myCommandHistory.data.reduce(function(prev, cur) {  return prev + cur[1];}, 0) / 1000);
+              console.log("[getCommandHistoryValue] " + type + " | from : " + from + " | to : " + to + " | value : " + myCommandHistory.data.reduce(function(prev, cur) {  return prev + cur[1];}, 0) / 1000);
+            }
         }
     });
 }
